d4f208dc1afecb84928ae28bf16e7f99276c9f78
albertorodrigoviu <95152985+albertorodrigoviu@users.noreply.github.com>
2021-11-28 12:28:05 +0100

    Update aplicacion_bbdd_nube.php

--- aplicacion_bbdd_nube.php
+++ aplicacion_bbdd_nube.php
@@ -1,26 +1,26 @@
 <?php
-	// << añadir conexión nube >>
+	// << aÃ±adir conexiÃ³n nube >>
     // ElephantSQL
-    // URL: postgres://<nombre_usuario>:<contraseña_usuario>@<nombre_maquina>/<nombre_bbdd>
-    // Datos de la cadena de conexión
-    $maquina = "tai.db.elephantsql.com"; // cambiar por la máquina que indica ElephantSQL
+    // URL: postgres://<nombre_usuario>:<contraseÃ±a_usuario>@<nombre_maquina>/<nombre_bbdd>
+    // Datos de la cadena de conexiÃ³n
+    $maquina = "tai.db.elephantsql.com"; // cambiar por la mÃ¡quina que indica ElephantSQL
     $puerto = "5432";
     $bbdd = "buqjeypq"; // cambiar por tu bbdd
     $usuario = "buqjeypq"; // cambiar por tu usuario
-    $contrasenya = "ZyRPZlzTrurmGNYkGuauAVfKbLZmAgum"; // cambiar por tu contraseña
-	// << añadir conexión nube >>
+    $contrasenya = "########"; // cambiar por tu contraseÃ±a
+	// << aÃ±adir conexiÃ³n nube >>
 
-    // Establecer la conexión a bbdd PostgreSQL
+    // Establecer la conexiÃ³n a bbdd PostgreSQL
     $conexion = pg_connect("host=$maquina port=$puerto dbname=$bbdd user=$usuario password=$contrasenya");
 
-    // Estado de la conexión
+    // Estado de la conexiÃ³n
     if (!$conexion) {
-        echo utf8_encode("Error: No se ha podido realizar la conexión.");
+        echo utf8_encode("Error: No se ha podido realizar la conexiÃ³n.");
         exit;
 	}
 
 // ------------ CONSULTAS -------------------------------------------------------------------
-	//<<<añadir consultaJOIN1>>>
+	//<<<aÃ±adir consultaJOIN1>>>
 	// Consulta JOIN 1
 	function consulta_join1($conexion) {
 	  // Realizar una consulta SQL
@@ -34,30 +34,30 @@
 	  }   
 	  // Comprobar que devuelve la consulta
 	  if (pg_num_rows($resultado) == 0) { // La consulta no tiene ninguna fila
-		 echo utf8_encode("No se encontró ninguna fila 	que coincida con la consulta join 1.");
+		 echo utf8_encode("No se encontrÃ³ ninguna fila 	que coincida con la consulta join 1.");
 	  } else {
-		 //<<<añadir mostrar consultaJOIN1>>>
+		 //<<<aÃ±adir mostrar consultaJOIN1>>>
 		 echo "<p>===================<br/>";  
 		 echo "CONSULTA JOIN 1<br/>";
 		 echo "===================<p/>";
 		 echo "<p>";
 		 // Mostrar los datos de la consulta
 		 while ($fila = pg_fetch_row($resultado)) {
-		   // Añadir todos los campos que se quieran mostrar  $fila[0] para  primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
+		   // AÃ±adir todos los campos que se quieran mostrar  $fila[0] para  primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
 		   echo "Campo 1: $fila[0] Campo 2: $fila[1]  Campo 3: $fila[2]";
 		   echo "<br />\n";
 		 }
 		 echo "===================<p/>";
-		 //<<<añadir mostrar consultaJOIN1>>>
+		 //<<<aÃ±adir mostrar consultaJOIN1>>>
 	  }
 	  // Liberar los resultados   
 	  pg_free_result($resultado);
 	}
 	// Realizar consultajoin1
 	consulta_join1($conexion);
-	//<<<añadir consultaJOIN1>>>
+	//<<<aÃ±adir consultaJOIN1>>>
   
-	//<<<añadir consultaJOIN2>>>
+	//<<<aÃ±adir consultaJOIN2>>>
 	// Consulta JOIN 2
 	function consulta_join2($conexion) {
 		// Realizar una consulta SQL
@@ -72,21 +72,21 @@
 		// Comprobar que devuelve la consulta
 		if (pg_num_rows($resultado) == 0) {
 			// La consulta no tiene ninguna fila
-			echo utf8_encode("No se encontró ninguna fila que coincida con la consulta join 2.");
+			echo utf8_encode("No se encontrÃ³ ninguna fila que coincida con la consulta join 2.");
 		} else {
-			//<<<añadir mostrar consultaJOIN2>>>
+			//<<<aÃ±adir mostrar consultaJOIN2>>>
 			echo "<p>===================<br/>";
 			echo "CONSULTA JOIN 2<br/>"; 
 			echo "===================<p/>";
 			echo "<p>";
 			// Mostrar los datos de la consulta
 			while ($fila = pg_fetch_row($resultado)) {
-			// Añadir todos los campos que se quieran mostrar  $fila[0] para primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
+			// AÃ±adir todos los campos que se quieran mostrar  $fila[0] para primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
 				echo "Campo 1: $fila[0] Campo 2: $fila[1]";
 				echo "<br />\n";
 			}
 			echo "===================<p/>";
-			//<<<añadir mostrar consultaJOIN2>>>
+			//<<<aÃ±adir mostrar consultaJOIN2>>>
 		}
 
 		// Liberar los resultados
@@ -94,8 +94,8 @@
 	}
     // Realizar consultajoin2
     consulta_join2($conexion);
-    //<<<añadir consultaJOIN2>>>
-    //<<<añadir consultaAGREGADA1>>>
+    //<<<aÃ±adir consultaJOIN2>>>
+    //<<<aÃ±adir consultaAGREGADA1>>>
     // Consulta AGREGADA 1
 
     function consulta_agregada1($conexion) {
@@ -112,30 +112,30 @@
 		// Comprobar que devuelve la consulta
 		if (pg_num_rows($resultado) == 0) {
            // La consulta no tiene ninguna fila
-           echo utf8_encode("No se encontró ninguna fila que coincida con la consulta agregada 1.");
+           echo utf8_encode("No se encontrÃ³ ninguna fila que coincida con la consulta agregada 1.");
         } else {
-           //<<<añadir mostrar AGREGADA1>>>
+           //<<<aÃ±adir mostrar AGREGADA1>>>
            echo "<p>===================<br/>";
            echo "CONSULTA AGREGADA 1<br/>";
            echo "===================<p/>";
            echo "<p>";
            // Mostrar los datos de la consulta
            while ($fila = pg_fetch_row($resultado)) {
-				// Añadir todos los campos que se quieran mostrar $fila[0] para primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
+				// AÃ±adir todos los campos que se quieran mostrar $fila[0] para primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
                 echo "Campo 1: $fila[0] Campo 2: $fila[1]";
                 echo "<br />\n";
            }
 		   echo "===================<p/>";
-		//<<<añadir mostrar consultaAGREGADA1>>>
+		//<<<aÃ±adir mostrar consultaAGREGADA1>>>
         }
         // Liberar los resultados  
         pg_free_result($resultado);
 	}
     // Realizar consultaAGREGADA1  
     consulta_agregada1($conexion);   
-    ////<<<añadir consultaAGREGADA1>>>
+    ////<<<aÃ±adir consultaAGREGADA1>>>
     
-	//<<<añadir consultaAGREGADA2>>>
+	//<<<aÃ±adir consultaAGREGADA2>>>
     // Consulta AGREGADA 2
     function consulta_agregada2($conexion) {
         // Realizar una consulta SQL
@@ -150,29 +150,29 @@
         // Comprobar que devuelve la consulta
         if (pg_num_rows($resultado) == 0) {
 			// La consulta no tiene ninguna fila
-			echo utf8_encode("No se encontró ninguna fila que coincida con la consulta agregada 2.");
+			echo utf8_encode("No se encontrÃ³ ninguna fila que coincida con la consulta agregada 2.");
         } else {
-			//<<<añadir mostrar consultaAGREGADA2>>>
+			//<<<aÃ±adir mostrar consultaAGREGADA2>>>
 			echo "<p>===================<br/>";
 			echo "CONSULTA AGREGADA 2<br/>";
 			echo "===================<p/>";
 			echo "<p>";
 			// Mostrar los datos de la consulta
 			while ($fila = pg_fetch_row($resultado)) {
-				// Añadir todos los campos que se quieran mostrar $fila[0] para primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
+				// AÃ±adir todos los campos que se quieran mostrar $fila[0] para primer campo, $fila[1] para segundo campo, $fila[2] para tercer campo, ...
 				echo "Campo 1: $fila[0] Campo 2: $fila[1]";
 				echo "<br />\n";
 			}
 			echo "===================<p/>";
-			//<<<añadir mostrar consultaAGREGADA2>>>
+			//<<<aÃ±adir mostrar consultaAGREGADA2>>>
 		}
 		// Liberar los resultados  
 		pg_free_result($resultado); 
 	} 
 	// Realizar consultaAGREGADA2    
 	consulta_agregada2($conexion);   
-	////<<<añadir consultaAGREGADA2>>> 
-// Cerrar la conexión
+	////<<<aÃ±adir consultaAGREGADA2>>> 
+// Cerrar la conexiÃ³n
 pg_close($conexion);
 
 ?>
